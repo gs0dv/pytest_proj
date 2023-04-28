7aa872ca42ea0b510110c6659158427a75df44fb
gs0dv <gs0dv@main.ru>
2023-04-28 14:47:19 +0300

    changed returned value from "None" to "git" in function get_val in file dicts.py, in connection with fixed func test_get_val in file test_dicts.py

--- tests/test_dicts.py
+++ tests/test_dicts.py
@@ -9,4 +9,4 @@ def test_get_val():
     assert get_val(data, "vcs", "git") == "mercurial"
     assert get_val({}, "vcs", "git") == "git"
     assert get_val({}, "vcs", "bazaar") == "bazaar"
-    assert get_val({}) == None
+    assert get_val({}) == 'git'
--- utils/dicts.py
+++ utils/dicts.py
@@ -1,5 +1,5 @@
 def get_val(collection, key=None, default='git'):
     if not key:
-        return None
+        return 'git'
 
     return collection[key] if collection else default
