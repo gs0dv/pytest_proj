19136b3b630feeccd605166e505620ea33bcc505
gs0dv <gs0dv@main.ru>
2023-04-28 15:10:36 +0300

    changed data from list to pytest.fixture in functions test_get and test_slice in file test_arrs.py, added file conftest.py

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -1,16 +1,17 @@
+from tests.conftest import array_fixture
 from utils import arrs
 
 
-def test_get():
-    assert arrs.get([1, 2, 3], 1, "test") == 2
+def test_get(array_fixture):
+    assert arrs.get(array_fixture, 1, "test") == 2
     assert arrs.get([], 0, "test") == "test"
 
 
-def test_slice():
-    assert arrs.my_slice([1, 2, 3, 4], 1, 3) == [2, 3]
-    assert arrs.my_slice([1, 2, 3], 1) == [2, 3]
-    assert arrs.my_slice([1, 2, 3, 4], end=1) == [1]
+def test_slice(array_fixture):
+    assert arrs.my_slice(array_fixture, 1, 3) == [2, 3]
+    assert arrs.my_slice(array_fixture, 1) == [2, 3, 4]
+    assert arrs.my_slice(array_fixture, end=1) == [1]
     assert arrs.my_slice([]) == []
-    assert arrs.my_slice([1, 2, 3, 4], -2) == [3, 4]
-    assert arrs.my_slice([1, 2, 3, 4], -3, 3) == [2, 3]
-    assert arrs.my_slice([1, 2, 3, 4], -30, 3) == [1, 2, 3]
+    assert arrs.my_slice(array_fixture, -2) == [3, 4]
+    assert arrs.my_slice(array_fixture, -3, 3) == [2, 3]
+    assert arrs.my_slice(array_fixture, -30, 3) == [1, 2, 3]
